3af96282f2704964e5a6d8da9b6f6d91e8f2607c
Avijit Paul <er.avijit18@gmail.com>
2023-10-10 10:43:55 +0600

    Add files via upload

---
+++ src/main/java/bjit/academy/DBOperations.java
@@ -0,0 +1,35 @@
+package bjit.academy;
+
+import java.sql.*;
+
+public class DBOperations {
+    Connection conn;
+    public void doConnectDB() {
+        String connectionStr = "jdbc:mysql://localhost:3306/"+Utils.DB_NAME;
+        String userName = "root";
+        String password = "";
+        try {
+            conn = DriverManager.getConnection(connectionStr, userName, password);
+            System.out.println("DB Connection is successful!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void fetchData() {
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM "+Utils.TABLE_NAME + "WHERE "
+                    + Utils.COLUMN_TEACHERNAME+ "like %Alex%");
+
+            while (rs.next()) {
+                String name = rs.getString(Utils.COLUMN_TEACHERNAME);
+                int age = rs.getInt(Utils.COLUMN_AGE);
+                String bloodGroup = rs.getString(Utils.COLUMN_BG);
+                System.out.println("Name: " + name + ", Age: " + age + ", Blood group: "+bloodGroup);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/bjit/academy/Utils.java
@@ -0,0 +1,11 @@
+package bjit.academy;
+
+public class Utils {
+
+    public static final String DB_NAME = "School";
+    public static final String TABLE_NAME = "teachers";
+    public static final String COLUMN_TEACHERNAME = "teacher_name";
+    public static final String COLUMN_AGE = "age";
+    public static final String COLUMN_BG = "blood_group";
+
+}
